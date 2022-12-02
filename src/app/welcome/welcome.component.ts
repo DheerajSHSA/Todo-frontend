48c2cf49f6d4bd536ce2842c7d4f6c039eb2b491
Dheeraj kashyap <Dheerajkashyap4889@gmail.com>
2022-12-02 11:26:57 +0530

    Restful Web Services- Get Requests

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { WelcomeDataService } from '../service/data/welcome-data.service';
 
 @Component({
   selector: 'app-welcome',
@@ -8,9 +9,15 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class WelcomeComponent implements OnInit {
   name = ''
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, public service: WelcomeDataService) { }
 
   ngOnInit(): void {
     this.name = this.route.snapshot.params['name']
+  }
+
+  getWelcomeMessage() {
+    this.service.executeHelloWorldBeanService().subscribe(
+      response => console.log(response.message)
+    );
   }
 }
